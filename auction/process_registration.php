c3d614e398f34931d2ab6dbed7a46f20167b120c
AnTienHuang <antien.huang@gmail.com>
2023-10-31 15:54:09 +0000

    prevent duplicate email and password mismatch

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -11,7 +11,8 @@
         $account_type = $_POST['accountType'];
         $email = mysqli_real_escape_string($con, $_POST['email']);
         $password = mysqli_real_escape_string($con, $_POST['password']);
-        $is_valid = true;
+        $password2 = mysqli_real_escape_string($con, $_POST['passwordConfirmation']);
+        $valid = true;
 
         foreach($_POST as $key => $value){
             echo"{$key} = {$value} <br>";
@@ -25,48 +26,32 @@
                     WHERE email = '$email'";
         $users = mysqli_query($con, $query);
         $row_cnt = mysqli_num_rows($users);
-        // printf("Select returned %d rows.\n", $row_cnt);
-        // foreach ($users as $user) {
-        //     printf("User ID: %s \n", $user["userId"]);
-        //     echo"<br>";
-        //     printf("User Email: %s \n", $user["email"]);
-        //     echo"<br>";
-        // }
+        // [DEBUG]
+            // printf("Select returned %d rows.\n", $row_cnt);
+            // foreach ($users as $user) {
+            //     printf("User ID: %s \n", $user["userId"]);
+            //     echo"<br>";
+            //     printf("User Email: %s \n", $user["email"]);
+            //     echo"<br>";
+            // }
         if($row_cnt > 0){
             $error = "User already exists";
-            $is_valid = false;
+            $valid = false;
         }
 
         # check if passwords are the same
+        if($password != $password2){
+            $error = "Passwords does not match";
+            $valid = false;
+        }
 
-
-
-
-            // date_default_timezone_set('Europe/London');
-            // $time = date('H:i:s', time());
+        # return error if invalid
+        if(!$valid){
+            header("Location: register.php?error=" . urldecode($error));
+            exit();
+        }
         
-            // if (!isset($user) || $user == '' || !isset($message) || $message == '') {
-            //   $error = "Please fill in your name and a message";
-            //   header("Location: index.php?error=" . urlencode($error));
-            //   exit();
-            // }
-            // else {
-            //   $query = "INSERT INTO messages (user, message, time) 
-            //             VALUES ('$user', '$message', '$time')";
-            //   if (!mysqli_query($connection, $query)) {
-            //     die('Error: ' . mysqli_error($connection));
-            //   }
-            //   else {
-            //     header('Location: index.php');
-            //     exit();
-            //   }
-            // }
-            if(!$is_valid){
-                header("Location: register.php?error=" . urldecode($error));
-                exit();
-            }
-            
-            echo"Is valid: {$is_valid}";
+        echo"Is valid: {$valid}";
     }
 
 ?>
