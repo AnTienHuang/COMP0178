4ddc8bf6886fac65a72c976c655b381876ff7318
AnTienHuang <antien.huang@gmail.com>
2023-10-31 11:58:11 +0000

    update db.sql

--- auction/db.php
+++ auction/db.php
@@ -6,11 +6,11 @@
     $con = "";
 
     // try{
-    $con = mysqli_connect($db_server,
-        $db_user,
-        $db_password,
-        $db_name
-    );
+    // $con = mysqli_connect($db_server,
+    //     $db_user,
+    //     $db_password,
+    //     $db_name
+    // );
 
     // if($con){
     //     echo"connected";
@@ -22,4 +22,12 @@
     // catch(){
     //     echo"there is an error"
     // }
+
+    $mysqli = new mysqli($db_server, $db_user, $db_password, $db_name);
+
+    // Check connection
+    if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+    }
 ?>
--- auction/process_registration.php
+++ auction/process_registration.php
@@ -15,47 +15,55 @@
         $is_valid = true;
 
         # check if there are missing values
-        
-        foreach($_POST as $key => $value){
-            echo"{$key} = {$value} <br>";
-            if(empty($value)){
-                $is_valid = false;
-            }
-        }
+            // (Done in the form)
+            //
+            // foreach($_POST as $key => $value){
+            //     echo"{$key} = {$value} <br>";
+            //     if(empty($value)){
+            //         $is_valid = false;
+            //     }
+            // }
+            //
 
-        if(!$is_valid){
-            $error = "Please fill in all required fields";
-            header("Location: register.php?error=" . urldecode($error));
-            exit();
-        }
 
-        // echo"Is valid: {$is_valid}";
         # check if user exists
+        $query = "SELECT userId
+                    FROM User
+                    WHERE userId = ''";
 
+        $user = $mysqli->query("SELECT id FROM test ORDER BY id ASC");
+
+        $error = "User already exists";
         # check if passwords are the same
 
 
 
 
-        // date_default_timezone_set('Europe/London');
-        // $time = date('H:i:s', time());
-    
-        // if (!isset($user) || $user == '' || !isset($message) || $message == '') {
-        //   $error = "Please fill in your name and a message";
-        //   header("Location: index.php?error=" . urlencode($error));
-        //   exit();
-        // }
-        // else {
-        //   $query = "INSERT INTO messages (user, message, time) 
-        //             VALUES ('$user', '$message', '$time')";
-        //   if (!mysqli_query($connection, $query)) {
-        //     die('Error: ' . mysqli_error($connection));
-        //   }
-        //   else {
-        //     header('Location: index.php');
-        //     exit();
-        //   }
-        // }
+            // date_default_timezone_set('Europe/London');
+            // $time = date('H:i:s', time());
+        
+            // if (!isset($user) || $user == '' || !isset($message) || $message == '') {
+            //   $error = "Please fill in your name and a message";
+            //   header("Location: index.php?error=" . urlencode($error));
+            //   exit();
+            // }
+            // else {
+            //   $query = "INSERT INTO messages (user, message, time) 
+            //             VALUES ('$user', '$message', '$time')";
+            //   if (!mysqli_query($connection, $query)) {
+            //     die('Error: ' . mysqli_error($connection));
+            //   }
+            //   else {
+            //     header('Location: index.php');
+            //     exit();
+            //   }
+            // }
+            if(!$is_valid){
+                header("Location: register.php?error=" . urldecode($error));
+                exit();
+            }
+            
+            echo"Is valid: {$is_valid}";
     }
 
 ?>
