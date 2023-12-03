13c5db8610d0c63bc19952cc2f9060041cf56bd1
AnTienHuang <antien.huang@gmail.com>
2023-12-03 22:50:10 +0000

    update sql

--- auction/sql/CREATE_TABLE_USER.sql
+++ auction/sql/CREATE_TABLE_USER.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS User;
 
 CREATE TABLE User
 (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id VARCHAR(255) PRIMARY KEY,
   firstName VARCHAR(40) NOT NULL,
   lastName VARCHAR(40) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(40) NOT NULL,
+  -- email VARCHAR(40) NOT NULL,
   -- isSeller BOOLEAN NOT NULL
   accountType VARCHAR(6) NOT NULL,
   CONSTRAINT check_account_type CHECK (accountType IN ('Seller', 'Buyer'))
--- auction/sql/CREATE_TABLE_WatchList.sql
+++ auction/sql/CREATE_TABLE_WatchList.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS WatchList;
 CREATE TABLE WatchList
 (
   itemId INTEGER NOT NULL,
-  userId INTEGER NOT NULL,
+  userId VARCHAR(255) NOT NULL,
   addedTime datetime NOT NULL,
   PRIMARY KEY (itemId, userId),
   KEY pkey (userId) -- create index on userId as well to improve performance
